115d3442165fc67549f055c7ad7dc22b28f5db7d
DendyA <githubmain_dja@gsi.bc.ca>
2022-06-20 16:06:16 -0700

    Added some testing data of receiving a POST request, changing the capacity and returning the correct format data. Based on the spec.

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
+
 # from TestExample.LinearRegTesting import *
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "Hello World!"
-    
-@app.route("/predict_class_size")
+
+
+@app.route("/predict_class_size", methods=["POST"])
 def predict_class_size():
     # Call function from other py file
     # Store and return the output from the function
-    return "Class Capacity: 45" + "\n\n" + "We did it!"
+
+    receivedJSON = request.json
+    print(receivedJSON[0])
+    receivedJSON[0]["capacity"] = 299
+
+    return jsonify(receivedJSON)
+    # return "Class Capacity: 45" + "\n\n" + "We did it!"
