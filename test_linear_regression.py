4fc78e4acccb891e4c683716db6596277cbb8cbd
Ethan McNamara <ethanmcnamara@uvic.ca>
2022-07-07 15:51:01 -0700

    added capacity prediction tests

--- test_linear_regression.py
+++ test_linear_regression.py
@@ -8,11 +8,11 @@ db_connection = sqlite.create_connection("database.sqlite")
 
 def test_linear_model():
     result = pytest.test_model.predict_size("test")
-    assert result.startswith('Predicted')
+    assert result.startswith('Predicted'), "linear regression model did not run properly"
 
 def test_db_connection():
     result = db_connection
-    assert result is not None
+    assert result is not None, "connection to SQLite DB does not exist"
 
 def test_courses_table_init():
     cur = db_connection.cursor()
@@ -22,7 +22,7 @@ def test_courses_table_init():
                     WHERE type='table' 
                     AND name='courses' ''')
     result = cur.fetchone()[0] == 1
-    assert result is True
+    assert result is True, "courses table does not exist"
 
 def test_enrollment_table_init():
     cur = db_connection.cursor()
@@ -32,7 +32,7 @@ def test_enrollment_table_init():
                     WHERE type='table' 
                     AND name='enrollment' ''')
     result = cur.fetchone()[0] == 1
-    assert result is True
+    assert result is True, "enrollment table does not exist"
 
 def test_coefficients_table_init():
     cur = db_connection.cursor()
@@ -42,7 +42,7 @@ def test_coefficients_table_init():
                     WHERE type='table' 
                     AND name='coefficients' ''')
     result = cur.fetchone()[0] == 1
-    assert result is True
+    assert result is True, "coefficients table does not exist"
 
 def test_courses_table_is_populated():
     cur = db_connection.cursor()
@@ -51,7 +51,7 @@ def test_courses_table_is_populated():
                     FROM `courses`
                     ''')
     result = cur.rowcount()
-    assert result > 0
+    assert result > 0, "courses table is not populated"
 
 def test_enrollment_table_is_populated():
     cur = db_connection.cursor()
@@ -60,7 +60,7 @@ def test_enrollment_table_is_populated():
                     FROM `enrollment`
                     ''')
     result = cur.rowcount()
-    assert result > 0
+    assert result > 0, "enrollment table is not populated"
 
 def test_coefficients_table_is_populated():
     cur = db_connection.cursor()
@@ -69,9 +69,32 @@ def test_coefficients_table_is_populated():
                     FROM `coefficients`
                     ''')
     result = cur.rowcount()
-    assert result > 0
+    assert result > 0, "coefficients table is not populated"
 
 #TODO:
     # figure out a way to test each course in the DB
     # get a list of their past capacities, and make an acceptable range
     # in which the predicted value can fall
+def get_average_capacity(course):
+    cur = db_connection.cursor()
+    cur.execute('''
+                    SELECT AVG(`size`)
+                    FROM `courses`
+                    WHERE `class_name` LIKE ?
+                ''', (course))
+    return cur.fetchall()[0]
+
+def test_capcity_preciction():
+    cur = db_connection.cursor()
+    cur.execute('''
+                    SELECT DISTINCT `class_name`
+                    FROM `courses`
+                    WHERE `size` > 0
+                ''')
+    course_list = cur.fetchall()
+    for course in course_list:
+        predicted_capacity = pytest.test_model.predict_size(course)
+        avg_capacity = get_average_capacity(course)
+        range_max = avg_capacity * 1.20
+        range_min = avg_capacity * 0.80
+        pytest.assume(predicted_capacity >= range_min and predicted_capacity <= range_max)
